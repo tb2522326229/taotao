0dd03141e1011335d1277c46d09999d4bcef9795
e <727056228@qq.com>
2018-05-21 08:07:02 +0800

    给商品详情页面添加缓存

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemDescServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemDescServiceImpl.java
@@ -2,21 +2,38 @@ package com.taotao.service.impl;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+import javax.jms.Destination;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.jedis.JedisClient;
 import com.taotao.mapper.TbItemDescMapper;
 import com.taotao.pojo.TbItemDesc;
 import com.taotao.pojo.TbItemDescExample;
 import com.taotao.pojo.TbItemDescExample.Criteria;
 import com.taotao.service.ItemDescService;
+import com.taotao.utils.JsonUtils;
 
 @Service
 public class ItemDescServiceImpl implements ItemDescService {
 
 	@Autowired
 	private TbItemDescMapper itemDescMapper;
+	@Resource(name="itemAddtopic")
+	private Destination destination;
+	@Autowired
+	private JedisClient jedisClient;
+	
+	@Value("${ITEM_INFO}")
+	private String ITEM_INFO;
+	@Value("${TIEM_EXPIRE}")
+	private Integer TIEM_EXPIRE;
 	@Override
 	public TaotaoResult getItemDesc(Long id) {
 		TbItemDescExample example = new TbItemDescExample();
@@ -27,6 +44,26 @@ public class ItemDescServiceImpl implements ItemDescService {
 	}
 	@Override
 	public TbItemDesc getItemDescById(Long itemId) {
+		//查询数据库之前先查询缓存
+		try {
+			String json = jedisClient.get(ITEM_INFO + ":" + itemId  + ":DESC");
+			if (StringUtils.isNotBlank(json)) {
+				// 把json数据转换成pojo
+				TbItemDesc tbItemDesc = JsonUtils.jsonToPojo(json, TbItemDesc.class);
+				return tbItemDesc;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		TbItemDesc itemDesc = itemDescMapper.selectByPrimaryKey(itemId);
+		try {
+			//把查询结果添加到缓存
+			jedisClient.set(ITEM_INFO + ":" + itemId  + ":DESC", JsonUtils.objectToJson(itemDesc));
+			//设置过期时间，提高缓存的利用率
+			jedisClient.expire(ITEM_INFO + ":" + itemId  + ":DESC", TIEM_EXPIRE);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return itemDescMapper.selectByPrimaryKey(itemId);
 	}
 }
--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemParamItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemParamItemServiceImpl.java
@@ -1,21 +1,51 @@
 package com.taotao.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.taotao.jedis.JedisClient;
 import com.taotao.mapper.TbItemParamItemMapper;
 import com.taotao.pojo.TbItemParamItem;
-import com.taotao.pojo.TbItemParamItemExample;
-import com.taotao.pojo.TbItemParamItemExample.Criteria;
 import com.taotao.service.ItemParamItemService;
+import com.taotao.utils.JsonUtils;
 
 @Service
 public class ItemParamItemServiceImpl implements ItemParamItemService {
 
 	@Autowired
 	private TbItemParamItemMapper paramItemMapper;
+	
+	@Autowired
+	private JedisClient jedisClient;
+	
+	@Value("${ITEM_INFO}")
+	private String ITEM_INFO;
+	@Value("${TIEM_EXPIRE}")
+	private Integer TIEM_EXPIRE;
 	@Override
 	public TbItemParamItem getParamById(Long itemId) {
+		//查询数据库之前先查询缓存
+		try {
+			String json = jedisClient.get(ITEM_INFO + ":" + itemId  + ":PARAM");
+			if (StringUtils.isNotBlank(json)) {
+				// 把json数据转换成pojo
+				TbItemParamItem itemParamItem = JsonUtils.jsonToPojo(json, TbItemParamItem.class);
+				return itemParamItem;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		TbItemParamItem itemParamItem = paramItemMapper.selectByPrimaryKey(itemId);
+		try {
+			//把查询结果添加到缓存
+			jedisClient.set(ITEM_INFO + ":" + itemId  + ":PARAM", JsonUtils.objectToJson(itemParamItem));
+			//设置过期时间，提高缓存的利用率
+			jedisClient.expire(ITEM_INFO + ":" + itemId  + ":DESC", TIEM_EXPIRE);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return paramItemMapper.selectByPrimaryKey(itemId);
 	}
 
