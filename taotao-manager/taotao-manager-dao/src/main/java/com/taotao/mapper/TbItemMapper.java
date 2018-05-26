ce03f242ee5c11ca40553cd916a7365c840b9f8e
e <727056228@qq.com>
2018-05-26 16:41:37 +0800

    重新生成一次pojo和mapper

---
+++ taotao-manager/taotao-manager-dao/src/main/java/com/taotao/mapper/TbItemMapper.java
@@ -0,0 +1,38 @@
+package com.taotao.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+
+public interface TbItemMapper {
+    int countByExample(TbItemExample example);
+
+    int deleteByExample(TbItemExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    /**
+     * 批量删除
+     * @param ids 传入要删除的商品id集合
+     * @return
+     */
+    int deleteBatch(@Param("ids") List<Long> ids);
+    
+    int insert(TbItem record);
+
+    int insertSelective(TbItem record);
+
+    List<TbItem> selectByExample(TbItemExample example);
+
+    TbItem selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbItem record, @Param("example") TbItemExample example);
+
+    int updateByExample(@Param("record") TbItem record, @Param("example") TbItemExample example);
+
+    int updateByPrimaryKeySelective(TbItem record);
+
+    int updateByPrimaryKey(TbItem record);
+}
