b5c4232af5ed5fa46561e5477854f79251fdbc3b
e <727056228@qq.com>
2018-05-28 16:14:10 +0800

    商品后台页面展示商品规格模板

---
+++ taotao-manager/taotao-manager-dao/src/main/java/com/taotao/mapper/TbItemParamAndCatMapper.java
@@ -0,0 +1,10 @@
+package com.taotao.mapper;
+
+import java.util.List;
+
+import com.taotao.pojo.TbItemParamAndCat;
+
+public interface TbItemParamAndCatMapper {
+
+	public List<TbItemParamAndCat> getItemParamList();
+}
---
+++ taotao-manager/taotao-manager-pojo/src/main/java/com/taotao/pojo/TbItemParamAndCat.java
@@ -0,0 +1,14 @@
+package com.taotao.pojo;
+
+public class TbItemParamAndCat extends TbItemParam{
+	private String itemCatName;
+
+	public String getItemCatName() {
+		return itemCatName;
+	}
+
+	public void setItemCatName(String itemCatName) {
+		this.itemCatName = itemCatName;
+	}
+	
+}
